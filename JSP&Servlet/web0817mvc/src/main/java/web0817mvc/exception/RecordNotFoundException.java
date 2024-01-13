4cd9da87d5914de6343715801ac4eae098ca4985
Dohyeon Kong <kkong_do@naver.com>
2024-01-13 18:45:45 +0900

    Add files via upload

---
+++ JSP&Servlet/web0817mvc/src/main/java/web0817mvc/exception/RecordNotFoundException.java
@@ -0,0 +1,11 @@
+package web0817mvc.exception;
+
+public class RecordNotFoundException extends Exception {
+
+	public RecordNotFoundException() {
+	}
+	public RecordNotFoundException(String message) {
+		super(message);
+	}
+
+}
